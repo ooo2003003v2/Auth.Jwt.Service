7fbe229931a3a77f340312062218747d1f56ede7
ooo2003003v2 <ooo2003003v2@gmail.com>
2020-11-24 15:14:20 +0800

    add label handling

---
+++ Auth.Service.Application/LabelManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Auth.Service.Persistence;
+using Auth.Service.Domain.Model.Request;
+using Auth.Service.Domain.Model;
+using Auth.Service.Domain.DBModel;
+using System.Security.Claims;
+using Newtonsoft.Json;
+using General.Auth.Account.Domain;
+using General.EF.Core.Function;
+namespace Auth.Service.Application
+{
+    public class LabelManager
+    {
+
+
+        
+
+        public static string GetLabel(string key)
+        {
+            return LabelPersistence.GetLabel(key)?.LabelAttr.FirstOrDefault()?.LabelDesc;
+        }
+
+
+    }
+}
---
+++ Auth.Service.Persistence/LabelPersistence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Auth.Service.Domain.DBModel;
+using General.EF.Core.Function;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Service.Persistence
+{
+   public class LabelPersistence
+    {
+        
+        public static Label GetLabel(string key)
+        {
+            Label label = new Label();
+            try
+            {
+                using (var db = new DBContext())
+                {
+
+                    label = db.Label
+                        .Where(p=>p.LabelKey == key)
+                        .Include(p=>p.LabelAttr)                        
+                        .FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex + "");
+
+            }
+            return label;
+        }
+    }
+}
